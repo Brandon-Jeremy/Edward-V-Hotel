afc8ccadf00ba698af11c20866c8339a9743c2d0
Brandon-Jeremy <brandonnader1@gmail.com>
2023-04-04 11:38:41 +0300

    Initial Commit
    Added laravel site with first online user table migration

---
+++ site/database/migrations/2023_04_04_083531_create_online_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOnlineUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('online_users', function (Blueprint $table) {
+            //Table important
+            $table->id();
+            //User important
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->date('dob');
+            $table->string('email')->unique();
+            $table->string('phone_num')->unique();
+            $table->string('password');
+            $table->string('billing_address');
+            $table->string('passport_id')->unique();
+            $table->rememberToken();
+            $table->timestamp('email_verified_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('online_users');
+    }
+}
