6059a35515202c69433ae563003e9241abfc0d43
Maher Mawlawi <maher.mawlawi@gmail.com>
2023-04-04 14:00:17 +0300

    added reedems table

---
+++ site/database/migrations/2023_04_04_105549_create_reedems_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReedemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reedems', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            //
+            $table->int('user_id');
+            $table->int('reward_id');
+            $table->boolean('reedemed');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reedems');
+    }
+}
