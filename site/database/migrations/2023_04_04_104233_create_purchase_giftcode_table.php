7b8c8158c7956e83b6b808f2d25ddc739776a18d
Brandon-Jeremy <brandonnader1@gmail.com>
2023-04-04 13:53:31 +0300

    Added purchase giftcode table as pivot table between giftcode and user for action purchase

---
+++ site/database/migrations/2023_04_04_104233_create_purchase_giftcode_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePurchaseGiftcodeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('purchase_giftcode', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            //Purchase giftcode table additions
+            $table->int('onlineuser_id');
+            $table->int('giftcard_id');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('purchase_giftcode');
+    }
+}
