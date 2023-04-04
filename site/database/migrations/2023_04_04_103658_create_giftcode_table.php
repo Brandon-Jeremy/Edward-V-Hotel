e6f9fb8bf9a1872786d1380c7e8919a4653a828b
Brandon-Jeremy <brandonnader1@gmail.com>
2023-04-04 13:41:31 +0300

    Added giftcode table

---
+++ site/database/migrations/2023_04_04_103658_create_giftcode_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGiftcodeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('giftcode', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            //giftcodes additions
+            $table->boolean('isRedeemed');
+            $table->string('token');
+            $table->int('value');
+            $table->timestamp('creation_date');
+            $table->timestamp('expiration_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('giftcode');
+    }
+}
