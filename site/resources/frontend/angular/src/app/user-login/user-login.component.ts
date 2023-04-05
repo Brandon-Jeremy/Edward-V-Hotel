0f88c9c0505719b16c846daaf22f4fa5e432d379
POENIXMAN <theahmad64@gmail.com>
2023-04-05 07:02:50 +0300

    added user-login files

---
+++ site/resources/frontend/angular/src/app/user-login/user-login.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-login',
+  templateUrl: './user-login.component.html',
+  styleUrls: ['./user-login.component.css']
+})
+export class UserLoginComponent {
+  signupUsers: any[] =[];
+  constructor() {}
+}
