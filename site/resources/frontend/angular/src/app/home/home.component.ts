c3e064019407f780960f1e8ce1d9f9e4106ec03c
Maher Mawlawi <maher.mawlawi@gmail.com>
2023-04-11 17:50:34 +0300

    fixed merge issue

--- site/resources/frontend/angular/src/app/home/home.component.ts
+++ site/resources/frontend/angular/src/app/home/home.component.ts
@@ -1,47 +1,31 @@
-import { Component, OnInit, ElementRef, HostListener } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  header!: ElementRef;
-  hamburgerMenu!: ElementRef;
-  showLoginForm = false;
-  showSignUpForm = false;
 
-  constructor(private elementRef: ElementRef) {}
+  constructor() { }
 
   ngOnInit(): void {
-    this.header = this.elementRef.nativeElement.querySelector('.header');
-    this.hamburgerMenu = this.elementRef.nativeElement.querySelector('.hamburger-menu');
-  }
+    const header = document.querySelector('.header');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
 
-  @HostListener('window:scroll', ['$event'])
-  onWindowScroll(): void {
-    const windowPosition = window.scrollY > 0;
-    this.header.nativeElement.classList.toggle('active', windowPosition);
-  }
+if (header && hamburgerMenu) {
+  window.addEventListener('scroll', function () {
+    const windowposition = this.window.scrollY > 0;
+    header.classList.toggle('active', windowposition);
+  });
 
-  onHamburgerMenuClick(): void {
-    const windowPosition = window.scrollY > 0;
-    this.header.nativeElement.classList.toggle('menu-open', windowPosition);
-  }
-
-  openLoginForm(){
-    this.showLoginForm = true;
-  }
-  
-  openSignUpForm(){
-    this.showSignUpForm = true;
-  }
-
-  closeLoginForm() {
-    this.showLoginForm = false;
-  }
-
-  closeSignUpForm() {
-    this.showSignUpForm = false;
-  }
-
+  hamburgerMenu.addEventListener('click', function(){
+    header.classList.toggle('menu-open');
+  });
 }
+
+
+    
+}
+}
+
