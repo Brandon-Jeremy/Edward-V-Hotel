f0b5206211a95481b4fd6a42a4efbc0709511b0a
Brandon-Jeremy <brandonnader1@gmail.com>
2023-04-11 15:45:44 +0300

    Online Reservation Controller skeleton, functions inside will need to be changed, waiting on Areeba response for payment processing API

---
+++ site/app/Http/Controllers/OnlineReservationController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Reservation;
+
+class OnlineReservationController extends Controller
+{
+
+    public function create()
+    {
+        // Code to show create reservation form
+    }
+
+    public function store(Request $request)
+    {
+        // Code to store the reservation
+    }
+
+    public function edit($id)
+    {
+        // Code to show edit reservation form
+    }
+
+    public function update(Request $request, $id)
+    {
+        // Code to update the reservation
+    }
+
+    public function destroy($id)
+    {
+        // Code to delete the reservation
+    }
+}
