4879926dc0afa655a7217f163a07ea36be135dd3
Maher Mawlawi <maher.mawlawi@gmail.com>
2023-04-11 13:53:43 +0300

    fixed home page

--- site/resources/frontend/angular/src/app/home/home.component.ts
+++ site/resources/frontend/angular/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ElementRef, HostListener } from '@angular/core';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { UserRegistrationComponent } from './user-registration/user-registration.component'; 
 
 @Component({
   selector: 'app-home',
@@ -26,4 +28,14 @@ export class HomeComponent implements OnInit {
     const windowPosition = window.scrollY > 0;
     this.header.nativeElement.classList.toggle('menu-open', windowPosition);
   }
+
+  openLoginForm(){
+
+  }
+  
+  openSignUpForm(){
+
+  }
+
+
 }
