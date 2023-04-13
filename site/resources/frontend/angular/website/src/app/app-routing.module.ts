aab830ccfefc705743b274e0b5341916df42d273
Maher Mawlawi <maher.mawlawi@gmail.com>
2023-04-13 18:31:52 +0300

    added rooms component

--- site/resources/frontend/angular/website/src/app/app-routing.module.ts
+++ site/resources/frontend/angular/website/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ReservationBarComponent } from './reservation-bar/reservation-bar.component';
 import { AboutComponent } from './about/about.component';
+import { RoomsComponent } from './rooms/rooms.component';
 
 const routes: Routes = [
   {path:"", component:HomeComponent},
   {path:"home", component:HomeComponent},
   {path:"reservation",component:ReservationBarComponent},
-  {path:"about",component:AboutComponent}
+  {path:"about",component:AboutComponent},
+  {path:"rooms",component:RoomsComponent},
 ];
 
 @NgModule({
--- site/resources/frontend/angular/website/src/app/app.component.ts
+++ site/resources/frontend/angular/website/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { ReservationBarComponent } from './reservation-bar/reservation-bar.component';
 import { AboutComponent } from './about/about.component';
+import { RoomsComponent } from './rooms/rooms.component';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +15,7 @@ export class AppComponent {
     { component: HomeComponent, className: 'home-section' },
     { component: ReservationBarComponent, className: 'reservation-bar-section' },
     { component: AboutComponent, className: 'about-section' },
+    { component: RoomsComponent, className: 'rooms-section' },
     // { component: FooterComponent, className: 'footer-section' }
   ];
 }
