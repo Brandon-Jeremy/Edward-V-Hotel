ec8b9c1fcd6a39b06b5ee82a8ea72cef09c875dc
Brandon-Jeremy <brandonnader1@gmail.com>
2023-04-16 15:49:53 +0300

    Updated the export PDF API to now show, the type of room rented, price per night, overall price, and other additional information

--- site/resources/views/reservations.blade.php
+++ site/resources/views/reservations.blade.php
@@ -24,24 +24,35 @@
 <body>
     <h1>Reservations Report</h1>
     <table>
-        <thead>
+    <thead>
+        <tr>
+            <th>Date From</th>
+            <th>Date To</th>
+            <th>Room Type</th>
+            <th>Room View</th>
+            <th>Room Number</th>
+            <th>Floor</th>
+            <th>Price per Night</th>
+            <th>Number of Nights</th>
+            <th>Total Price</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($reservations as $reservation)
             <tr>
-                <th>Room</th>
-                <th>Floor</th>
-                <th>Date From</th>
-                <th>Date To</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($reservations as $reservation)
-            <tr>
-                <td>{{ $reservation->room_number }}</td>
-                <td>{{ $reservation->floor }}</td>
                 <td>{{ $reservation->date_from }}</td>
                 <td>{{ $reservation->date_to }}</td>
+                <td>{{ $reservation->type }}</td>
+                <td>{{ $reservation->view }}</td>
+                <td>{{ $reservation->room_number }}</td>
+                <td>{{ $reservation->floor }}</td>
+                <td>{{ $reservation->price }}</td>
+                <td>{{ $reservation->num_nights }}</td>
+                <td>{{ $reservation->total_price }}</td>
             </tr>
-            @endforeach
-        </tbody>
-    </table>
+        @endforeach
+    </tbody>
+</table>
+
 </body>
 </html>
