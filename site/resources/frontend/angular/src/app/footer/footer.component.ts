14679c69144d35be87ac29d6d6d7c44bb92d0718
Maher Mawlawi <maher.mawlawi@gmail.com>
2023-04-24 22:32:24 +0300

    added scroll to section functionality to footer

--- site/resources/frontend/angular/src/app/footer/footer.component.ts
+++ site/resources/frontend/angular/src/app/footer/footer.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
 
+  scrollToSection(sectionId: string) {
+    const element = document.querySelector(sectionId);
+    if (element) {
+      const top = element.getBoundingClientRect().top + window.pageYOffset;
+      window.scrollTo({ top, behavior: 'smooth' });
+    }
+  }
+  
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
 }
