543728ea1b196f17b76fb7b060a249ed95d38cb2
Maher Mawlawi <maher.mawlawi@gmail.com>
2023-04-29 11:30:47 +0300

    added available rooms routing

--- site/resources/frontend/angular/src/app/app-routing.module.ts
+++ site/resources/frontend/angular/src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { RewardComponent } from './reward/reward.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { UserComponent } from './user/user.component';
 import { AuthGuard } from './auth.guard';
+import { AvailableRoomsComponent } from './available-rooms/available-rooms.component';
 
 //might be used later
 // import { AboutComponent } from './about/about.component';
@@ -19,9 +20,8 @@ import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'user', component: UserComponent, canActivate: [AuthGuard] },
-  //{path: '**', redirectTo: '' }, // redirect to home if the requested URL doesn't match any path
+   //{path: '**', redirectTo: '' }, // redirect to home if the requested URL doesn't match any path
+  { path:"home", component: HomeComponent },
   {path:"login", component: LoginFormComponent },
   {path:"signup", component: SignUpFormComponent },
   {path:"book", component:BookingComponent},
@@ -29,7 +29,9 @@ const routes: Routes = [
   {path:"contact", component:ContactComponent},
   {path:"rewards", component:RewardComponent},
   {path:"user", component: UserComponent, canActivate: [AuthGuard] },
-  {path:"forgot-password", component:ForgotPasswordComponent}
+  {path:"forgot-password", component:ForgotPasswordComponent},
+  {path:"available-rooms", component:AvailableRoomsComponent},
+  
 ];
 
 @NgModule({
