0a3e5a4c899a8f12e64b80dac85eb32a615e2701
Maher Mawlawi <maher.mawlawi@gmail.com>
2023-04-29 20:35:58 +0300

    fixed room routing

--- site/resources/frontend/angular/src/app/rooms/rooms.component.ts
+++ site/resources/frontend/angular/src/app/rooms/rooms.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { LoginFormComponent } from '../login-form/login-form.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-rooms',
@@ -6,5 +11,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./rooms.component.css']
 })
 export class RoomsComponent {
+
+  constructor(private authService:AuthService, private router:Router, private snackBar:MatSnackBar, private dialog:MatDialog) {}
+
+  isUserLoggedIn(): boolean {
+    console.log('authStatus:', this.authService.getIsAuthenticated().getValue());
+    return this.authService.getIsAuthenticated().getValue();
+  }
+
+  navigateToPayment(): void {
+    if (this.isUserLoggedIn() == true) {
+      this.router.navigate(['/payment']);
+    } else {
+      this.showSnackbar('Please login or register to book online');
+      this.openLoginForm();
+    }
+  }
+
+  showSnackbar(message: string): void {
+    this.snackBar.open(message, 'Close', {
+      duration: 3000,
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+    });
+  }
+
+  openLoginForm(): void {
+    this.dialog.open(LoginFormComponent, {
+      disableClose: true,
+      width: '600px', // Adjust the width
+      height: '500px', // Adjust the height
+    });
+  }
 
 }
