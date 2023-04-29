f781f60b746343fa0ea564524c76d837f3f0ce92
Maher Mawlawi <maher.mawlawi@gmail.com>
2023-04-30 00:16:25 +0300

    completed payment page

--- site/resources/frontend/angular/src/app/payment/payment.component.ts
+++ site/resources/frontend/angular/src/app/payment/payment.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
+import { ErrorDialogComponent } from '../error-dialog/error-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 
 @Component({
@@ -8,25 +10,39 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./payment.component.css']
 })
 export class PaymentComponent {
-  paymentForm!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
-    this.createForm();
+  cardName:string = '';
+  cardNumber:string = '';
+  expirationMonth:string = '';
+  expirationYear:string = '';
+  cvv:string = '';
+
+  nameFormControl = new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]);
+  cardNumberFormControl = new FormControl('', [Validators.required, Validators.minLength(16), Validators.maxLength(16), Validators.pattern('^[0-9]*$')]);
+  expirationMonthFormControl = new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(2), Validators.pattern('^[0-9]*$'), Validators.min(1), Validators.max(12)]);
+  expirationYearFormControl = new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(4), Validators.pattern('^[0-9]*$'), Validators.min(new Date().getFullYear()), Validators.max(new Date().getFullYear() + 10)]);
+  cvvFormControl = new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(4), Validators.pattern('^[0-9]*$')]);
+
+
+  constructor(private dialog:MatDialog) {
   }
 
-  createForm() {
-    this.paymentForm = this.formBuilder.group({
-      nameOnCard: ['', Validators.required],
-      cardNumber: ['', Validators.required],
-      expirationMonth: ['', Validators.required],
-      expirationYear: ['', Validators.required],
-      cvv: ['', Validators.required]
-    });
-  }
 
   onSubmit() {
-    console.log(this.paymentForm.value);
+    if(this.cardName.length <= 0 || this.cardNumber.length <= 0 || this.expirationMonth.length <= 0 || this.expirationYear.length <=0 || this.cvv.length <= 0){
+      this.showErrorDialog("Missing field(s)");
+      return;
+    }
+
+
     // Send payment information to API or backend for processing
+  }
+
+
+  private showErrorDialog(errorMessage: string): void {
+    this.dialog.open(ErrorDialogComponent, {
+      data: { errorMessage },
+    });
   }
 
   
