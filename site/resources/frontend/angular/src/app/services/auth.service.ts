8e9caffdf83d5e789b87e4899ebf9d43bc62b3e5
Maher Mawlawi <maher.mawlawi@gmail.com>
2023-04-29 21:37:57 +0300

    fixed linking for login

--- site/resources/frontend/angular/src/app/services/auth.service.ts
+++ site/resources/frontend/angular/src/app/services/auth.service.ts
@@ -1,83 +1,53 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Observable, of, BehaviorSubject } from 'rxjs';
-import { tap, catchError, delay } from 'rxjs/operators';
-import { MatDialog } from '@angular/material/dialog';
-import { ErrorDialogComponent } from '../error-dialog/error-dialog.component';
-
-import * as z from 'zod';
-
-
-const emailSchema = z.string().email({ message: 'Please enter a valid email address' });
-const passwordSchema = z.string().min(8, { message: 'Password must be at least 8 characters long' });
-const nameRegex = /^[A-Za-z\- ]+$/;
-const firstNameSchema = z.string().regex(nameRegex, { message: 'First name must consists of only alphabets' }).min(2, { message: 'First name must be at least 2 characters long' });
-const lastNameSchema = z.string().regex(nameRegex, { message: 'Last name must consists of only alphabets' }).min(2, { message: 'Last name must be at least 2 characters long' });
-const digitsRegex = /^\d+$/;
-const numberSchema = z.string().regex(digitsRegex, { message: 'Please enter a valid phone number' });
-const confirmPasswordSchema = z.string().min(8, { message: 'Confirm password must be at least 8 characters long' });
-
-const userLoginSchema = z.object({
-  email: emailSchema,
-  password: passwordSchema,
-});
-
-const userSignUpSchema = z.object({
-  email: emailSchema,
-  password: passwordSchema,
-  firstName: firstNameSchema,
-  lastName: lastNameSchema,
-  number: numberSchema,
-  confirmPassword: confirmPasswordSchema,
-  }).refine(
-    (data) => data.password === data.confirmPassword,
-    { message: 'Passwords do not match' }
-);
-
+import { tap, delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:4200/auth';
+  private loginUrl = 'http://localhost:4200/api/login-user';
+  private signupUrl = 'http://localhost:4200/api/register-user';
 
   private isAuthenticated = new BehaviorSubject<boolean>(false);
   authStatus$ = this.isAuthenticated.asObservable();
 
-  constructor(private http: HttpClient, private dialog: MatDialog) {}
-
+  constructor() {}
 
   login(email: string, password: string): Observable<any> {
-    try {
-      userLoginSchema.parse({ email, password });
-    } catch (error) {
-      if (error instanceof z.ZodError) {
-        const errorMessage = error.errors[0].message;
-        console.log(error);
-        this.showErrorDialog(errorMessage || 'An error occurred');
-        throw new Error(errorMessage);
-      }
-    }
-
-    return this.http.post(`${this.apiUrl}/login`, { email, password }).pipe(
-      tap((response) => {
-        // Handle successful login, e.g., store the user data, token, etc.
-        this.onSuccessfulLogin(response);
-        console.log('isAuthenticated:', this.isAuthenticated.value); // add this line
-      }),
-      catchError((err) => {
-        console.log(err);
-        throw err;
+    const requestBody = {
+      email: email,
+      password: password
+    };
+  
+    return new Observable(observer => {
+      fetch(this.loginUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: JSON.stringify(requestBody)
       })
-    );
-  }
-
-  private showErrorDialog(errorMessage: string): void {
-    this.dialog.open(ErrorDialogComponent, {
-      data: { errorMessage },
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Failed to login');
+        }
+      })
+      .then(data => {
+        this.onSuccessfulLogin(data);
+        this.setAuthenticated(true);
+        observer.next(data);
+        observer.complete();
+      })
+      .catch(error => {
+        console.error(error);
+        observer.error(error);
+      });
     });
   }
-
 
   fakeLogin(email: string, password: string): Observable<any> {
     const userData = {
@@ -102,31 +72,6 @@ export class AuthService {
     );
   }
 
-  signUp(email: string, password: string, firstName: string, lastName: string, number: string, confirmPassword: string): Observable<any> {
-    try {
-      userSignUpSchema.parse({ email, password, firstName, lastName, number, confirmPassword });
-      if (password !== confirmPassword) {
-        throw new Error('Password and confirm password do not match');
-      }
-    } catch (error) {
-      if (error instanceof z.ZodError) {
-        const errorMessage = error.errors[0].message;
-        console.log(error);
-        this.showErrorDialog(errorMessage || 'An error occurred');
-        throw new Error(errorMessage);
-      } else {
-        throw error;
-      }
-    }
-
-    return this.http.post(`${this.apiUrl}/signup`, { email, password, firstName, lastName, number }).pipe(
-      catchError((err) => {
-        console.log(err);
-        throw err;
-      })
-    );
-  }
-
 
   onSuccessfulLogin(response: any): void {
     // Store user data, token, etc., as needed
@@ -146,7 +91,6 @@ export class AuthService {
   isLoggedIn(): boolean {
     return this.isAuthenticated.getValue();
   }
-  
   
 
   logout() {
